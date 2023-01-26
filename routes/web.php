76fe0cf9a30d2fbf2e4e2e8f47245a0bdbb95b96
Bishoy Romany <bishoy@queensenglish.co>
2023-01-26 18:37:53 +0200

    Use Factory

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::resource('products', \App\Http\Controllers\ProductController::class);
+Route::resource('products', \App\Http\Controllers\ProductController::class)->middleware('auth');
 
 require __DIR__.'/auth.php';
